4c6c99e09a8781b63c396e8301f859903b26118b
Sofia Katzman <katzmansof@gmail.com>
2023-03-02 20:32:02 -0500

    Finished Lab

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
+
+
+let totalBatteries = batteryBatches.reduce(function (accumulator, element){
+    return element + accumulator;
+}, 0);
+/*
 // Code your solution here
+let totalBatteries = batteryBatches.reduce(function (countObj, currentBat) {
+    if (currentBat in countObj) {
+        countObj[currentBat]++;
+    } else {
+        countObj[currentBat] = 1;
+    }
+    return countObj;
+}, {});
+
+console.log(totalBatteries);
+
+*/
